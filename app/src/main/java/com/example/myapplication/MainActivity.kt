4d3fed50cea753b8c6017e31c02141c04460c599
ArtGODD <arturcedaspy@gmail.com>
2019-11-06 22:00:40 -0300

    Telas

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnGerenciar.setOnClickListener {
+            val gerenciar = Intent(this,GerenciarActivity::class.java)
+            startActivity(gerenciar)
+        }
+
+        btnListar.setOnClickListener {
+            val listar = Intent(this,ListarActivity::class.java)
+            startActivity(listar)
+        }
+
     }
 }
