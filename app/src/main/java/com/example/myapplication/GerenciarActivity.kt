8cf97cfc9b0deea52b39eb9ba673694ad16ebdd1
Art <arturcedaspy@gmail.com>
2019-11-13 21:22:58 -0300

    13/11

--- app/src/main/java/com/example/myapplication/GerenciarActivity.kt
+++ app/src/main/java/com/example/myapplication/GerenciarActivity.kt
@@ -1,12 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_gerenciar.*
 
 class GerenciarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gerenciar)
+
+        btnCadastrar.setOnClickListener {
+            val cadastrar = Intent(this, CadastrarActivity::class.java)
+            startActivity(cadastrar)
+        }
+
+        btnEditar.setOnClickListener {
+            val editar = Intent (this, EditarActivity::class.java)
+            startActivity(editar)
+        }
     }
 }
